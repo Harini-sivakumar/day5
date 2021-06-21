dd7ac96fad47cd9929f9c620b05499b124c8c503
Harini_sivakumar <harini@techbreakthrough.co.in>
2021-06-21 13:26:03 +0530

    forms completed

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -18,6 +18,10 @@ paddress:any;
 caddress:any;
 hmarks!:number;
 hpercent!:number;
+smarks!:number;
+spercent!:number;
+birth!:number;
+showAge!:number;
 
 
 constructor(private formbuilder:FormBuilder) { }
@@ -47,11 +51,19 @@ get f() { return this.student.controls; }
  
 Address(){
   if(this.caddress){
-    this.paddress=this.caddress;
-    console.log(this.paddress);
+    this.student.controls.PERMANENTADDRESS.setValue(this.caddress);
+    console.log(this.student.controls.PERMANENTADDRESS.value);
   }
   
 
+}
+ageCalc(){
+  if(this.birth){
+    const convertbirth = new Date(this.birth);
+    const timeDiff = Math.abs(Date.now() - convertbirth.getTime());
+    this.showAge = Math.floor((timeDiff / (1000 * 3600 * 24))/365.25);
+    this.student.controls.age.setValue(this.showAge)
+  }
 }
 hresult(){
   if(this.hmarks<=1200){
@@ -59,6 +71,14 @@ hresult(){
     console.log(this.hpercent);
   }else{
     this.hpercent=0;
+  }
+}
+sresult(){
+  if(this.smarks<=1200){
+    this.spercent=(((this.smarks)/500)*100);
+    console.log(this.spercent);
+  }else{
+    this.spercent=0;
   }
 }
 onSubmit(){
@@ -72,7 +92,7 @@ onSubmit(){
 
   console.log(this.student.value);
 
-   console.log(this.student.controls.PERCENT.value);
+   
  
 
 }
--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template.component.css']
 })
 export class TemplateComponent implements OnInit {
-
+submitted=false;
+model:any={}
   constructor() { }
 
   ngOnInit(): void {
   }
+   
+Address(){
+  if(this.model.CURRENTADDRESS){
+    this.model.PERMANENTADDRESS=this.model.CURRENTADDRESS;
+    console.log(this.model.PERMANENTADDRESS);
+  }
+}
 
+  hresult(){
+    if(this.model.HSLCMARK<=1200){
+      this.model.HSLCPERCENT=(((this.model.HSLCMARK)/1200)*100);
+      console.log(this.model.HSLCPERCENT);
+    }else{
+      this.model.HSLCPERCENTt=0;
+    }
+  } 
+  
+  
+  sresult(){
+    if(this.model.SSLCMARK<=1200){
+      this.model.SSLCPERCENT=(((this.model.SSLCMARK)/500)*100);
+      console.log(this.model.SSLCPERCENT);
+    }else{
+      this.model.HSLCPERCENTt=0;
+    }
+  } 
+  
+
+
+  onSubmit(){
+    this.submitted=true;
+  
+    if(this.model.invalid){
+      return;
+  
+    }
+    console.log(this.model);
+
+}
 }
